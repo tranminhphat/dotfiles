2299393cf4ce05f1423bdf96adfd66e33df00584
Phat Tran <phattran@kobiton.com>
2022-12-25 00:51:31 +0700

    update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,15 +5,23 @@ nvm use stable
 eval (/usr/local/bin/brew shellenv)
 fish_vi_key_bindings
 
-# Base16 Shell
-if status --is-interactive
-  set BASE16_SHELL "$HOME/.config/base16-shell/"
-  source "$BASE16_SHELL/profile_helper.fish"
-end
-
 # Alias
 alias c='clear'
 alias g='git'
 alias vi='nvim'
-alias glog='git log --graph --oneline --decorate --all'
-alias gsync="git checkout $argv && git fetch upstream && git rebase upstream/$argv && git push"
+alias gst='git status'
+alias gad='git add .'
+
+# Git
+function glog
+  git log --graph --oneline --decorate --all
+end
+
+function gsync
+  git checkout $argv && git fetch upstream && git rebase upstream/$argv && git push
+end
+
+function gcm
+  git commit -m $argv
+end
+
