3f37e71e342370b509c98a03ab1c5315c3bf492a
Phat Tran <phat.tranminh@employmenthero.com>
2023-05-04 00:28:00 +0700

    update

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -36,10 +36,10 @@ saga.setup({
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>sb', "<cmd>Lspsaga show_buf_diagnostics<CR>")
+vim.keymap.set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+vim.keymap.set('n', '[d', "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+vim.keymap.set('n', ']d', "<cmd>Lspsaga diagnostic_jump_next<CR>")
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -52,14 +52,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf, noremap = true, silent = true }
-    vim.keymap.set('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
-    vim.keymap.set('n', 'gh', '<Cmd>Lspsaga hover_doc<CR>', opts)
+    vim.keymap.set('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.keymap.set('n', 'gd', "<cmd>Lspsaga goto_definition<CR>", opts)
     vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
-    vim.keymap.set('n', '<leader>ld', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'gt', '<cmd>Lspsaga peek_type_definition<CR>', opts)
+    vim.keymap.set('n', 'gh', '<Cmd>Lspsaga hover_doc<CR>', opts)
+    vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
     vim.keymap.set('n', '<leader>lr', "<Cmd>Lspsaga rename<CR>", opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>la', "<Cmd>Lspsaga code_action<CR>", opts)
     vim.keymap.set('n', '<leader>lf', function()
